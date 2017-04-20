67d7b65859140861a78261935d6a87fdfe9c30b3
Aparna Kulkarni <aparna15kulkarni@gmail.com>
2017-04-19 22:41:42 -0700

    All Changes

--- src/main/java/ntrusted/controllers/UserController.java
+++ src/main/java/ntrusted/controllers/UserController.java
@@ -1,13 +1,11 @@
 package ntrusted.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import ntrusted.models.User;
 import ntrusted.models.UserDao;
